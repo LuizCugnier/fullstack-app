462db494e7808b45f5fd75745fe5a70a3a17bef0
Luiz Cugnier <luizcugnier.neto@gmail.com>
2023-09-26 07:56:22 -0300

    navega para o post escolhido ao clickar

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,30 @@
 import axios from "axios";
 import { useEffect, useState, React } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./Home.css";
 
 const Home = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
+  let navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("http://localhost:3000/posts").then((response) => {
+    axios.get("http://localhost:3306/posts").then((response) => {
       setListOfPosts(response.data);
     });
   }, []);
 
   return (
     <div className="App">
-        <h1>Posts</h1>
+      <h1>Posts</h1>
       {listOfPosts.map((value, key) => {
         return (
-          <div className="post">
+          <div
+            className="post"
+            onClick={() => {
+              navigate(`/post/${value.id}`);
+            }}
+          >
             <div className="title">{value.title}</div>
             <div className="body">{value.postText}</div>
             <div className="footer">@{value.username}</div>
