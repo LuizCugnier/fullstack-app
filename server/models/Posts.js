10621ac2486017f3f8ade1534c2d23f9295116d7
Luiz Cugnier <luizcugnier.neto@gmail.com>
2023-09-28 15:00:22 -0300

    att

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,19 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-    const Posts = sequelize.define('Posts', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        postText: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-    });
-
-    return Posts
-}
+  return Posts;
+};
