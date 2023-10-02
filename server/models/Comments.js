be68d331cd18992dedc438117f71d66b07311c68
Luiz Cugnier <luizcugnier.neto@gmail.com>
2023-10-01 23:35:25 -0300

    Model Comments created

---
+++ server/models/Comments.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comments = sequelize.define("Comments", {
+      commentBody: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+  
+    return Comments;
+  };
+  
